a8fc045148e6dd81cc97167e1b900adf74030c87
Jonathan Percy <jonathan@ledgerscope.com>
2017-07-11 13:03:36 +0100

    Added support for dates when retrieving Trial Balance report

--- FreeAgent/Client/AccountingClient.cs
+++ FreeAgent/Client/AccountingClient.cs
@@ -17,17 +17,35 @@ namespace FreeAgent
 		}
 
 
-		public List<TrialBalanceSummary> TrialBalanceSummary()
-		{
-			var request = CreateBasicRequest(Method.GET, "/trial_balance/summary");
-			var response = Client.Execute<TrialBalanceSummaryWrapper>(request);
+		public List<TrialBalanceSummary> TrialBalanceSummary(DateTime? from = null, DateTime? to = null)
+        {
+            var path = getTrialBalanceRequestPath(from, to);
+            var request = CreateBasicRequest(Method.GET, path);
+            var response = Client.Execute<TrialBalanceSummaryWrapper>(request);
 
-			if (response != null) return response.trial_balance_summary;
+            if (response != null) return response.trial_balance_summary;
 
-			return null;
+            return null;
 
-		}
+        }
 
-	}
+        private static string getTrialBalanceRequestPath(DateTime? from, DateTime? to)
+        {
+            var path = "/trial_balance/summary";
+            var fromDateParam = "from_date";
+            var toDateParam = "to_date";
+
+            if (from.HasValue && to.HasValue) return $"{path}?{fromDateParam}={format(from)}&{toDateParam}={format(to)}";
+            if (from.HasValue) return $"{path}?{fromDateParam}={format(from)}";
+            if (to.HasValue) return $"{path}?{toDateParam}={format(to)}";
+
+            return path;
+        }
+
+        private static string format(DateTime? date)
+        {
+            return date.Value.ToString("yyyy-MM-dd");
+        }
+    }
 }
 
